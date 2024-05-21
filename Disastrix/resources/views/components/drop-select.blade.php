38ce64dd9136fb644af8638d1e52314810c918b5
Gendi kinji <kithinjigeorge192@gmail.com>
2024-05-21 11:13:30 +0300

    Added dropdown value reading

--- Disastrix/resources/views/components/drop-select.blade.php
+++ Disastrix/resources/views/components/drop-select.blade.php
@@ -1,5 +1,5 @@
-<select {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
- <option>Public user</option>
- <option>Volunteer</option>
- <option>First respondent</option>
+<select id="role" name="role" {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+ <option value="public user">Public user</option>
+ <option value="volunteer">Volunteer</option>
+ <option value="respondent">First respondent</option>
 </select>
