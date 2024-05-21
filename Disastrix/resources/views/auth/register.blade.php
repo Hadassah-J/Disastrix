41385cd2d26c4d17f46c9ff0310122b1b72bf0f6
Hadassah-J <131357108+Hadassah-J@users.noreply.github.com>
2024-05-21 11:37:27 +0300

    Login page and registration

--- Disastrix/resources/views/auth/register.blade.php
+++ Disastrix/resources/views/auth/register.blade.php
@@ -61,4 +61,8 @@
             </div>
         </form>
     </x-authentication-card>
+    <script>
+    document.body.style.backgroundImage = "url('/images/Emergency.jpg')";
+    document.body.style.backgroundSize = "cover";
+</script>
 </x-guest-layout>
