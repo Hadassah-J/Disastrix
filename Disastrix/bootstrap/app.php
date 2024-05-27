be9531e835ebb3977c66ade2b7d0427a3d84c00d
Gendi kinji <kithinjigeorge192@gmail.com>
2024-05-27 21:51:13 +0300

    Added corrections

--- Disastrix/bootstrap/app.php
+++ Disastrix/bootstrap/app.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Middleware\LockMiddleware;
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\LockMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
--- Disastrix/routes/web.php
+++ Disastrix/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 use App\Http\Controllers\LockScreenController;
-use App\Http\Middleware\CheckIfLocked;
+use App\Http\Middleware\LockMiddleware;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,7 +15,7 @@ Route::middleware([
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
-    })->name('dashboard');
+    })->name('dashboard')->middleware((LockMiddleware::class));
 });
 Route::get('/lock', [LockScreenController::class, 'show'])->name('lock');
 Route::post('/unlock', [LockScreenController::class, 'unlock'])->name('unlock');
