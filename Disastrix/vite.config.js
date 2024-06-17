519a163627614f5b8fde4e834d2087283515b500
Gendi kinji <kithinjigeorge192@gmail.com>
2024-06-17 20:13:03 +0300

    Added admin dashboard

--- Disastrix/vite.config.js
+++ Disastrix/vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/all.min.css',
+                'resources/css/adminlte.min.css',
+                'resources/js/jquery.min.js',
+                'resources/js/bootstrap.bundle.min.css',
+                'resources/js/adminlte.js'
+
             ],
             refresh: true,
         }),
