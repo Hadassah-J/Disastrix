c48c6add3ed8c9bc2a887613c48d4196e072f078
Gendi kinji <kithinjigeorge192@gmail.com>
2024-06-22 21:50:38 +0300

    Added permission tables

--- Disastrix/database/migrations/0001_01_01_000000_create_users_table.php
+++ Disastrix/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,11 +11,35 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+
+        /*Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('role_name');
             $table->integer('number');
+        });*/
+        $teams = config('permission.teams');
+        $tableNames = config('permission.table_names');
+        $columnNames = config('permission.column_names');
+        $pivotRole = $columnNames['role_pivot_key'] ?? 'role_id';
+        $pivotPermission = $columnNames['permission_pivot_key'] ?? 'permission_id';
+
+
+        Schema::create($tableNames['roles'], function (Blueprint $table) use ($teams, $columnNames) {
+            $table->bigIncrements('id'); // role id
+            if ($teams || config('permission.testing')) { // permission.testing is a fix for sqlite testing
+                $table->unsignedBigInteger($columnNames['team_foreign_key'])->nullable();
+                $table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
+            }
+            $table->string('name');       // For MySQL 8.0 use string('name', 125);
+            $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
+            $table->timestamps();
+            if ($teams || config('permission.testing')) {
+                $table->unique([$columnNames['team_foreign_key'], 'name', 'guard_name']);
+            } else {
+                $table->unique(['name', 'guard_name']);
+            }
         });
+
         // Create users table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
