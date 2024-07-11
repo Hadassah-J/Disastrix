c1fb226c6d7ecead7e7c9a58a616aa09900b9b29
Gendi kinji <kithinjigeorge192@gmail.com>
2024-07-11 20:39:29 +0300

    Added report incident button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,11 +24,14 @@
             @livewire('navigation-menu')
 
             <div class="flex justify-end px-4 py-2 bg-white shadow">
+                <a href="{{route('incident-report')}}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                    Report incident
+                </a>
                 <button id="lock-screen-button" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
                     Lock Screen
                 </button>
             </div>
-            
+
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
